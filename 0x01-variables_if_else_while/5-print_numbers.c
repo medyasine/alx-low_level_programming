caae4e068300a826524a63f31658b797a651c6f0
medysine <yassinmohamad89@gmail.com>
2023-03-17 14:50:48 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int nb;
+nb = 0;
+while (nb < 10)
+{
+printf("%d", nb);
+nb++;
+}
+printf("\n");
+return (0);
+}
