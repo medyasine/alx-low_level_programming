a8d62a33d70890d98db3f722922fcdf9de04cd4c
medysine <yassinmohamad89@gmail.com>
2023-03-23 16:15:12 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,17 @@
 #include "main.h"
+
 /**
  * print_line - that draws a straight line in the terminal
  * @n: number as parameter
  * Return: void
  */
+
 void print_line(int n)
+
 {
+
 int l;
+
 if (n <= 0)
 {
 _putchar('\n');
